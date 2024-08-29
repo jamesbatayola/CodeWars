9532e22c0704225b0a3f30f6feab929c67b4a7e6
Jake James Batayola <batayolajakejames@gmail.com>
2024-08-30 01:37:54 +0800

    8KYU_03 | String repeat

---
+++ 8KYU_03/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _8KYU_03;
+
+//Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string asd = "";
+
+        Console.WriteLine(asd + "s");
+    }
+
+    public static string RepeatStr(int n, string s)
+    {
+        //string result = "";
+
+        //for (int i = 0; i < n; i++)
+        //    result += s;
+
+        return string.Join("", Enumerable.Range(1, n).Select(x => s).ToArray());
+    }
+
+}
+
+// COMPLETED
