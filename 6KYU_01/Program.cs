46da3eabca6d1a3a53d32d2a3608b7d4d1b5f5fb
Jake James Batayola <batayolajakejames@gmail.com>
2024-08-30 14:33:39 +0800

    6KYU_01

---
+++ 6KYU_01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _6KYU_01;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        IEnumerable<int> asd = new int[] { 11, 11, 14, 11, 11};
+
+        Console.WriteLine(GetUnique(asd));
+
+        Console.ReadLine();
+    }
+
+    // SOLUTION
+    public static int GetUnique(IEnumerable<int> numbers)
+    {
+
+        var collection = new List<int>(numbers);
+
+        var unique = collection
+                    .GroupBy(x => x)
+                    .OrderBy(x => x.Count())
+                    .Select(x => x.Key)
+                    .ToArray();
+
+        return unique[0];
+    }
+
+
+}
