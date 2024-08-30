032df032ebd447203a3a633b53cebf5a040240eb
Jake James Batayola <batayolajakejames@gmail.com>
2024-08-30 18:33:12 +0800

    8KYU_05

---
+++ 8KYU_05/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _8KYU_05;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Correct("hell0"));
+
+        Console.ReadLine();
+    }
+
+    // SOLUTION
+    public static string Correct(string text)
+    {
+        var revise = text.Select(ii => ii switch
+        {
+            '5' => "s",
+            '0' => "o",
+            '1' => "i",
+            _ => ii.ToString()
+        });
+
+        return string.Join("", revise);
+
+    }
+
+}
