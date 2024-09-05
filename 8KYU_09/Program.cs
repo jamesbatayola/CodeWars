20afb86d51681b90f6a8f5ae5e80be36e0f1e7f8
Jake James Batayola <batayolajakejames@gmail.com>
2024-09-05 14:00:40 +0800

    8KYU_09

---
+++ 8KYU_09/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _8KYU_09;
+
+// Is the string uppercase?
+// Task
+// Create a method to see whether the string is ALL CAPS.
+
+// Examples(input -> output)
+// "c" -> False
+// "C" -> True
+// "hello I AM DONALD" -> False
+// "HELLO I AM DONALD" -> True
+// "ACSKLDFJSgSKLDFJSKLDFJ" -> False
+// "ACSKLDFJSGSKLDFJSKLDFJ" -> True
+
+// In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.ReadLine();
+    }
+
+    // SOLUTION
+    public static bool _IsUpperCase(this string text)
+    {
+        return text.Where(x => char.IsLetter(x)).All(x => char.IsUpper(x));
+    }
+}
