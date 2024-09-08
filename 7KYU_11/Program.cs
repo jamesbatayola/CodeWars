e1badc404e067d1e1e5749e7d7c1cd279d83d7d0
Jake James Batayola <batayolajakejames@gmail.com>
2024-09-08 12:54:24 +0800

    7KYU_11

---
+++ 7KYU_11/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _7KYU_11;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(NbYear(1500, 5, 100, 5000));
+     
+        Console.ReadLine();
+    }
+
+
+    // SOLUTION
+    public static int NbYear(int p0, double percent, int aug, int p)
+    {
+        int res = 0;
+        while (p0 < p)
+        {
+            double added = (p0 * (percent * 0.01) + aug) + p0;
+            p0 = (int)added;
+            res++;
+        }
+        return res;
+    }
+
+}
