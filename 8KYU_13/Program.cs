70187bb201bc2576d684c81e43e7a6d8738706fd
Jake James Batayola <batayolajakejames@gmail.com>
2024-09-08 11:57:39 +0800

    8KYU_14

---
+++ 8KYU_13/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _8KYU_13;
+
+//You will be given a list of strings.You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+//The returned value must be a string, and have "***" between each of its letters.
+
+//You should not remove or add elements from/to the array.
+
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        var asd = new string[] { "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps" };
+
+//        //var ii = string.Join("***", asd.Order().First().ToCharArray());
+
+//        char.IsUpper
+
+//        //foreach(var i in ii)
+//        //    Console.WriteLine(i);
+
+//        Console.WriteLine(TwoSort(asd));
+
+//        Console.ReadLine();
+//    }
+
+//    // SOLUTION
+//    public static string TwoSort(string[] s)
+//    {
+//        return string.Join("***", s.OrderBy(x => x.ToCharArray().Select(x => char.IsUpper(x)));
+//    }
+
+//}
