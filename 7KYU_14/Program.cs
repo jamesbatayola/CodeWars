1e9b27b93202b3799e549887c31ef37c8a22c42d
Jake James Batayola <batayolajakejames@gmail.com>
2024-09-10 19:45:45 +0800

    7KYU_14

---
+++ 7KYU_14/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _7KYU_14;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string asd = "Hello World!!";
+        
+        Console.WriteLine(asd.Remove(asd.IndexOf("W")));
+
+        Console.ReadLine();
+    }
+
+    // SOLUTION
+    public static string RemoveUrlAnchor(string url)
+    {
+        return url.Contains("#") ? url.Remove(url.IndexOf("#")) : url;
+    }
+
+}
