4e32560ceceb8d70c188ba8d5a3de75c4b484bd1
Jake James Batayola <batayolajakejames@gmail.com>
2024-09-20 17:11:40 +0800

    6KYU_22

---
+++ 6KYU_22/Program.cs
@@ -0,0 +1,75 @@
+﻿// The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+// Examples
+
+// "din"      =>  "((("
+// "recede"   =>  "()()()"
+// "Success"  =>  ")())())"
+// "(( @"     =>  "))((" 
+
+// Notes
+
+// Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
+
+using System.Text;
+
+//string word = "din";
+
+//for (int i = 0; i < word.Length * word.Length; i++)
+//{
+//    Console.WriteLine($"outer: {i / word.Length} = {word[i / word.Length]} | inner: {i % word.Length} = {word[i % word.Length]}");
+//}
+
+Console.WriteLine(_DuplicateEncode("din"));
+Console.WriteLine(_DuplicateEncode("recede"));
+Console.WriteLine(_DuplicateEncode("Success"));
+Console.WriteLine(_DuplicateEncode("(( @"));
+
+// BRUTE FOREC APPROACH 
+static string DuplicateEncode(string word)
+{
+    var res = new StringBuilder();
+
+    int len = word.Length;
+
+    for (int i = 0; i < len; i++)
+    {
+        bool hasCopy = false;
+
+        for (int j = 0; j < len; j++)
+        {
+            if (i != j && char.ToLower(word[i]) == char.ToLower(word[j])) hasCopy = true;
+        }
+
+        if (!hasCopy) res.Append("(");
+        else res.Append(")");
+    }
+
+    return res.ToString();
+}
+
+// OTHER ANSWER ----------
+
+static string _DuplicateEncode(string word)
+{
+    var res = "";
+    bool isOnce = true;
+    int len = word.Length;
+
+    for (int i = 0; i < len * len; i++)
+    {
+        if ((i / len) != (i % len) && char.ToLower(word[i / len]) == char.ToLower(word[i % len]))
+        {
+            isOnce = false;
+        }
+
+        if(i % len == len - 1)
+        {
+            res += (i % len) == (len - 1) && isOnce ? "(" : ")";
+            isOnce = true;
+        }
+    }
+
+    return res;
+}
+
+Console.ReadLine();
---
+++ 7KYU_24/Program.cs
@@ -0,0 +1,28 @@
+﻿//Description:
+
+//Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+//Examples:
+
+//Input: 42145 Output: 54421
+
+//Input: 145263 Output: 654321
+
+//Input: 123456789 Output: 987654321
+//Fundamentals
+
+Console.WriteLine(DescendingOrder(42145));
+
+static int DescendingOrder(int num)
+{
+    int[] nums = num.ToString().Select(e => e - '0').ToArray();
+    return Convert.ToInt32(string.Join("", nums.OrderByDescending(e => e).ToArray()));
+}
+
+// OTHER ANSWERS -------------
+
+static int _DescendingOrder(int num)
+{
+    return int.Parse(string.Concat(num.ToString().OrderByDescending(x => x)));
+}
+
+Console.ReadLine();
