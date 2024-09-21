971ed1e993ab7e8ffb9bc0c23dd0d1bd5182ec9f
Jake James Batayola <batayolajakejames@gmail.com>
2024-09-22 03:59:17 +0800

    6KYU_23

---
+++ 6KYU_23/Program.cs
@@ -0,0 +1,33 @@
+﻿// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+// Examples
+
+// [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
+
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+// Examples
+
+// [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
+
+Console.WriteLine(__Find(new int[] { 2, 4, 0, 100, 4, 11, 2602, 36 }));
+Console.WriteLine(__Find(new int[] { 160, 3, 1719, 19, 11, 13, -21}));
+Console.WriteLine(__Find(new int[] { 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 }));
+
+static int __Find(int[] integers)
+{
+    var odd = new List<int>();
+    var even = new List<int>();
+
+    foreach (var i in integers)
+    {
+        if (i % 2 == 0) odd.Add(i);
+        else even.Add(i);
+    }
+
+    return odd.Count() > even.Count() ? even[0] : odd[0];
+}
+
+Console.ReadLine();
---
+++ 7KYU_25/Program.cs
@@ -0,0 +1,25 @@
+﻿// using static System.Runtime.InteropServices.JavaScript.JSType;
+// Description:
+
+// Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+// Examples:
+
+// divisors(12)-- > [2, 3, 4, 6]
+// divisors(25)-- > [5]
+// divisors(13)-- > "13 is prime"
+
+var a1 = Divisors(15);
+var a2 = Divisors(16);
+var a3 = Divisors(253);
+var a4 = Divisors(24);
+
+foreach(int i in a1)
+    Console.WriteLine(i);
+
+static int[] Divisors(int n)
+{
+    int[] res = Enumerable.Range(2, n - 2).Where(e => n % e == 0).ToArray();
+    return res.Length > 0 ? res : null;
+}
+
+Console.ReadLine();
