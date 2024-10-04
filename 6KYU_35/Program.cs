2e2a1b61a78ae35e435bda39968415f2e03d5958
Jake James Batayola <batayolajakejames@gmail.com>
2024-10-04 13:46:38 +0800

    7KYU_33

---
+++ 6KYU_35/Program.cs
@@ -0,0 +1,43 @@
+﻿// Write Number in Expanded Form
+
+// You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+// expandedForm(12)-- > should return "10 + 2"
+// expandedForm(42)-- > should return "40 + 2"
+// expandedForm(70304)-- > should return "70000 + 300 + 4"
+
+// NOTE: All numbers will be whole numbers greater than 0.
+
+using System.Text;
+
+Console.WriteLine(ExpandedForm(12));
+Console.WriteLine(ExpandedForm(43));
+Console.WriteLine(ExpandedForm(70304));
+
+static string ExpandedForm(long num)
+{ 
+    var digits = num.ToString();
+    var sb = new StringBuilder();
+
+    for (int i = 0; i < digits.Length; i++)
+    {
+        string temp = "";
+
+        temp += digits[i] != '0' && i != digits.Length ? $"{digits[i]} + {new string('0', digits.Length - i + 1)}" :
+                                    
+
+        if (digits[i] != '0' && i + 1 != digits.Length)
+        {
+            temp += 
+            //sb.Append(numString[i]);
+            //sb.Append('0', numString.Length - (i + 1));
+            //sb.Append(" + ");
+        }
+        else if (digits[i] != '0' && i + 1 == digits.Length)
+            sb.Append(digits[i]);
+    }
+
+    return sb.ToString();
+}
+
+Console.ReadLine();
---
+++ 7KYU_33/Program.cs
@@ -0,0 +1,25 @@
+﻿// Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+// For example:
+
+//    capitalize("abcdef", [1, 2, 5]) = "aBCdeF"
+//    capitalize("abcdef", [1, 2, 5, 100]) = "aBCdeF".There is no index 100.
+
+// The input will be a lowercase string with no spaces and an array of digits.
+
+// Good luck!
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+Console.WriteLine(Capitalize("qwertyuiopasd", [1, 2, 10]));
+Console.WriteLine(Capitalize("abcdef", [1, 2, 5, 100]));
+Console.WriteLine(Capitalize("qweasdzxcrtyfgh", [0]));
+
+// CONCISE VERSION
+static string Capitalize(string s, List<int> idxs)
+{
+    return string.Join("", s.Select((e, i) => idxs.Contains(i) ? char.ToUpper(e) : e));
+}
+
+Console.ReadLine();
