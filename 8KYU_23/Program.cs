d00c9bdd898cb4e5eaafff7f05330fca009f23c9
Jake James Batayola <batayolajakejames@gmail.com>
2024-10-20 13:41:50 +0800

    8KYU_23

---
+++ 8KYU_23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Description:
+
+// The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+// But it's not working properly.
+// Task
+
+// Fix the bug so we can all go home early.
+// Notes
+
+// String str will never be null.
+
+using System.Text.RegularExpressions;
+
+Console.WriteLine(ReplaceDots("one.two.three"));
+
+static string ReplaceDots(string str)
+{
+    return str.Replace('.', '-');
+}
+
+Console.ReadLine();
