fa5084662de9c20c93aa1a0aadde50fc1e400861
Jake James Batayola <batayolajakejames@gmail.com>
2024-10-31 18:08:21 +0800

    6KYU_45

---
+++ 6KYU_45/Program.cs
@@ -0,0 +1,34 @@
+﻿// Your task, is to create N×N multiplication table, of size provided in parameter.
+//
+//     For example, when given size is 3:
+//
+// 1 2 3
+// 2 4 6
+// 3 6 9
+//
+// For the given example, the return value should be:
+//
+// [[1,2,3],[2,4,6],[3,6,9]]
+
+var test1 = MultiplicationTable(3);
+
+foreach(var i in test1)
+    Console.WriteLine(i);
+
+static int[,] MultiplicationTable(int size)
+{
+    int[,] table = new int[size, size];
+    
+    for (int row = 0; row < size; row++)
+    {
+        int temp = row + 1;
+        for (int column = 0; column < size; column++)
+        {
+            table[row,column] = temp;
+            temp += row + 1;
+        }
+    }
+    
+    return table;
+}
+
--- practice/Program.cs
+++ practice/Program.cs
@@ -4,40 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
-            // LOOP =========================================================
+            string[,] matrix = { { "a", "b", "c" }, { "d", "e", "f" }, { "g", "h", "i" } };
 
-            // for loop ----------------------------------
-
-            // ex #1
-            //for(int i = 0; i < 5; i++)
-            //{
-            //    Console.WriteLine("Hello world");
-            //}
-
-            //for(int i = 0; i < 3; i++)
-            //{
-            //    Console.WriteLine("Hello world");
-            //}
-
-            // foreach loop -----------------------------
-
-            //string word = "example";
-
-            //foreach (var letter in word)
-            //{
-            //    Console.WriteLine(letter);
-            //}
-
-
-            Console.WriteLine("HELLO_WORLD");
-
-            for(int i = 1; i <= 5; i++)
-                Console.WriteLine($"{i}");
+            Console.WriteLine(matrix[0,0]);
 
             Console.ReadLine();
         }
-
     }
-
 }
