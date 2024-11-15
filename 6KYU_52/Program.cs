15959ab19c8550c4b2a74662dc39bec32310421c
Jake James Batayola <batayolajakejames@gmail.com>
2024-11-15 15:51:30 +0800

    6KYU_52

--- 6KYU_52/Program.cs
+++ 6KYU_52/Program.cs
@@ -25,25 +25,79 @@
 //D          500
 //M          1,000
 
+Console.WriteLine(Solution(1));
+Console.WriteLine(Solution(2));
+Console.WriteLine(Solution(4));
+Console.WriteLine(Solution(500));
+Console.WriteLine(Solution(1000));
+Console.WriteLine(Solution(1954));
+Console.WriteLine(Solution(1990));
+Console.WriteLine(Solution(2008));
+Console.WriteLine(Solution(2014));
+
+Console.WriteLine(Solution(45));
+
 static string Solution(int n)
 {
-    string res = "";
-    string year = n.ToString();
+    string result = "";
 
-    for(int i = 0; i < year.Length; i++)
+    var dict = new Dictionary<int, char>()
     {
-        if (year[i] != '0')
+        { 1, 'I' },
+        { 5, 'V' },
+        { 10, 'X' },
+        { 50, 'L' },
+        { 100, 'C' },
+        { 500, 'D' },
+        { 1000, 'M' }
+    };
+
+    int[] asd = [1000, 500, 100, 50, 10, 5, 1];
+
+    string yearString = n.ToString();
+
+    int yearSubstring = yearString[0] - '0';
+
+    for (int i = 0; i < asd.Length; i++)
+    {
+        yearSubstring = yearString.Substring(0).Length > 1 ?
+            int.Parse($"{yearString[0]}" + new string('0', yearString.Length - 1)) :
+            int.Parse(string.Join("", yearString.Where(e => e != 0)));
+
+        if (yearString[0] == '0')
         {
-            year.Substring(i).Length switch
+            continue;
+        }
+
+        if (asd[i] <= yearSubstring)
+        {
+            if ((yearString[0] - '0') + 1 == 10 || (yearString[0] - '0') + 1 == 5)
             {
-                4 => res += 'a',
-                3 => res += 's',
-                2 => res += 'd',
-                1 => res += 'z',
-                _ => res += 'z'
+                result += yearSubstring switch
+                {
+                    900 => "CM",
+                    400 => "CD",
+                    90 => "XC",
+                    40 => "XL",
+                    9 => "IX",
+                    4 => "IV",
+                    _ => ""
+                };
+
+                if (i < asd.Length - 1) i++;
             }
+            else
+            {
+                result += new string(dict[asd[i]], yearSubstring / asd[i]);
+            }
+
+            yearString = yearSubstring / asd[i] == 1 ?
+              $"{int.Parse(yearString) - asd[i]}" :
+              $"{int.Parse(yearString) - yearSubstring}";
         }
     }
 
-    return "asd";
+    return result;
 }
+
+// UNFINISHED
