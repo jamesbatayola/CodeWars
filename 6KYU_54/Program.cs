41a1f15f3fb39d2b87d1f63fbb5b10e3cb530eae
Jake James Batayola <batayolajakejames@gmail.com>
2024-11-17 21:11:06 +0800

    7KYU_58

---
+++ 6KYU_54/Program.cs
@@ -0,0 +1,21 @@
+﻿//Description:
+
+//When provided with a String, capitalize all vowels
+
+//For example:
+
+//Input: "Hello World!"
+
+//Output: "HEllO WOrld!"
+
+//Note: Y is not a vowel in this kata.
+
+
+using System.Text;
+
+static string Swap(string s)
+{
+    var res = s.Select(e => "aeiou".Contains(e, StringComparison.CurrentCultureIgnoreCase) ? char.ToUpper(e) : e);
+
+    return string.Join("", res);
+}
---
+++ 7KYU_58/Program.cs
@@ -0,0 +1,20 @@
+﻿//Description:
+
+//When provided with a String, capitalize all vowels
+
+//For example:
+
+//Input: "Hello World!"
+
+//Output: "HEllO WOrld!"
+
+//Note: Y is not a vowel in this kata.
+
+Console.WriteLine(Swap("Hello World!"));
+
+static string Swap(string s)
+{
+    var res = s.Select(e => "aeiou".Contains(e, StringComparison.CurrentCultureIgnoreCase) ? char.ToUpper(e) : e);
+
+    return string.Join("", res);
+}
