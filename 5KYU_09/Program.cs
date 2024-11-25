a725a15c7b3a610c6939decd64f1112c7c671a6a
Jake James Batayola <batayolajakejames@gmail.com>
2024-11-25 13:43:32 +0800

    5KYU_09 & 6KYU_63

---
+++ 5KYU_09/Program.cs
@@ -0,0 +1,41 @@
+﻿// Complete the function that
+//
+// accepts two integer arrays of equal length
+//     compares the value each member in one array to the corresponding member in the other
+// squares the absolute value difference between those two values
+//     and returns the average of those squared absolute value difference between each member pair.
+//     Examples
+//         [1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
+//     [10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
+//     [-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2
+
+Console.WriteLine(Solution([1, 2, 3], [4, 5, 6]));
+Console.WriteLine(Solution([10, 20, 10, 2], [10, 25, 5, -2] ));
+Console.WriteLine(Solution([-1, 0], [0, -1]));
+
+static double Solution(int[] firstArray, int[] secondArray)
+{
+    var o = new int[firstArray.Length];
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        int min = firstArray[i] > secondArray[i] ? secondArray[i] : firstArray[i];
+        int max = firstArray[i] < secondArray[i] ? secondArray[i] : firstArray[i];
+        int count = 0;
+        while (min < max)
+        {
+            min++;
+            count++;
+        }
+
+        o[i] = count * count;
+    }
+
+    return (double)o.Sum() / firstArray.Length;
+}
+
+// compares the value difference in each member of 2 arrays
+//  - create a count var
+//  - increment count by 1 for each digit differences
+// squares that count var by itself
+// sum count var for each member
+// divide the sum value by array length
---
+++ 6KYU_63/Program.cs
@@ -0,0 +1,42 @@
+﻿// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+//
+//     For example:
+//
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+//
+//     dup(["kelless","keenness"]) = ["keles","kenes"].
+//
+//     Strings will be lowercase only, no spaces. See test cases for more examples.
+//
+//     Good luck!
+//
+//     If you like this Kata, please try:
+//
+// Alternate capitalization
+//
+// Vowel consonant lexicon
+
+var test1 = dup(["abracadabra","allottee","assessee"]);
+var test2 = dup(["kelless","keenness"]);
+
+foreach(var each in test2)
+    Console.WriteLine(each);
+
+static string[] dup(string[] arr)
+{
+    var res = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        string temp = $"{arr[i][0]}";
+        for (int j = 1; j < arr[i].Length; j++)
+        {
+            temp += arr[i][j] != arr[i][j - 1] ? arr[i][j] : "";
+        }
+        res[i] = temp;
+    }
+    return res;
+}
+
+// loop through array
+// remove consecutive duplicate from each item and store them in temp variable
+// ater each item is filtered add the temp variable to the res variable
